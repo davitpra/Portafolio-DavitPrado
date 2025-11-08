d6f78bea405dc20a312cc89eb585663573cd6326
David Prado <j.david.prado1716@gmail.com>
2025-11-07 22:46:44 -0500

    change of layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,12 +7,11 @@ import '@/styles/tailwind.css'
 
 export const metadata: Metadata = {
   title: {
-    template: '%s - Spencer Sharp',
-    default:
-      'Spencer Sharp - Software designer, founder, and amateur astronaut',
+    template: 'David Prado',
+    default: 'David Prado - Full Stack Developer and Entrepreneur',
   },
   description:
-    'I’m Spencer, a software designer and entrepreneur based in New York City. I’m the founder and CEO of Planetaria, where we develop technologies that empower regular people to explore space on their own terms.',
+    'David Prado is a Full Stack Developer and Entrepreneur. I am passionate about building products and services that help people.',
   alternates: {
     types: {
       'application/rss+xml': `${process.env.NEXT_PUBLIC_SITE_URL}/feed.xml`,
